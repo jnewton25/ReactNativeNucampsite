2d2330ac8ab1b93b1ff4ba4b3076b0c347482b3c
Justin Newton <justin-newton@outlook.com>
2022-04-22 12:40:08 -0400

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Card } from "react-native-elements";
 import { ScrollView, Text } from "react-native";
 import { CAMPSITES } from "../shared/campsites";
+import * as Animatable from "react-native-animatable";
 
 class ContactUs extends Component {
 	constructor(props) {
@@ -18,18 +19,27 @@ class ContactUs extends Component {
 	render() {
 		const { navigate } = this.props.navigation;
 		return (
-			<ScrollView>
-				<Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
-					<Text>1 Nucamp Way</Text>
-					<Text>Seattle, WA 98001</Text>
-					<Text>U.S.A.</Text>
-					<Text></Text>
-					<Text>Phone: 1-206-555-1234</Text>
-					<Text style={{ marginBottom: 10 }}>
-						Email: campsites@nucamp.co
-					</Text>
-				</Card>
-			</ScrollView>
+			<Animatable.View
+				animation="fadeInDown"
+				duration={2000}
+				delay={1000}
+			>
+				<ScrollView>
+					<Card
+						title="Contact Information"
+						wrapperStyle={{ margin: 20 }}
+					>
+						<Text>1 Nucamp Way</Text>
+						<Text>Seattle, WA 98001</Text>
+						<Text>U.S.A.</Text>
+						<Text></Text>
+						<Text>Phone: 1-206-555-1234</Text>
+						<Text style={{ marginBottom: 10 }}>
+							Email: campsites@nucamp.co
+						</Text>
+					</Card>
+				</ScrollView>
+			</Animatable.View>
 		);
 	}
 }
